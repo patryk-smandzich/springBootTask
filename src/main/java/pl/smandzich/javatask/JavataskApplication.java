5ab41b3d940965f4dd45af1134427a5909237307
Patryk <hbsmandzich@gmail.com>
2020-01-27 13:29:26 +0100

    Basic structure

---
+++ src/main/java/pl/smandzich/javatask/JavataskApplication.java
@@ -0,0 +1,13 @@
+package pl.smandzich.javatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavataskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavataskApplication.class, args);
+    }
+
+}
